96eb82b6eb9315e4aaf8753dcb6d6c389a59fd81
fjrosselot <fjrosselot@gmail.com>
2026-04-19 15:53:45 -0400

    fix: move Toaster to main.tsx, was incorrectly added to unused App.jsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
 import { queryClient } from '@/lib/queryClient'
 import { router } from '@/router'
 import './index.css'
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster position="bottom-center" richColors />
     </QueryClientProvider>
   </React.StrictMode>,
 )
